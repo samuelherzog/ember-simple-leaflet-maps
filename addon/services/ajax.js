982881bac2cf2a383a28b8c476991e08b8f5844f
Samuel Herzog <samuel.herzog@salzburg.at>
2019-05-15 21:46:58 +0200

    Soften jQuery dependency

--- addon/services/ajax.js
+++ addon/services/ajax.js
@@ -1,5 +1,31 @@
-import AjaxService from 'ember-ajax/services/ajax'
+import Service from '@ember/service';
+import require, { has } from 'require';
 
-export default AjaxService.extend({
-  host: 'https://api.mapbox.com'
+let _fetch = function () {
+  throw new Error("It seems you have neither fetch nor jQuery available");
+}
+
+if (has('fetch')) {
+  // use `fetch` module by default, this is commonly provided by ember-fetch
+  _fetch = require('fetch').default;
+} else if (typeof fetch === 'function') {
+  // fallback to using global fetch
+  _fetch = fetch;
+} 
+
+const hasJQuery = typeof jQuery !== 'undefined';
+
+export default Service.extend({
+  host: 'https://api.mapbox.com',
+
+  async request(endpoint) {
+    let url = this.host + endpoint;
+
+    if (hasJQuery) {
+      return jQuery.getJSON(url);
+    } else {
+      let response = await _fetch(url);
+      return response.json();
+    }
+  }
 });
