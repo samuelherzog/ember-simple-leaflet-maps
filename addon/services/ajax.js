72c2d3d72b3a01eed3708a6b33ef1e6608745f44
Samuel Herzog <samuel.herzog@salzburg.at>
2019-05-15 22:02:24 +0200

    Use ember-ajax (again) instead of relying on plain jQuery

--- addon/services/ajax.js
+++ addon/services/ajax.js
@@ -1,19 +1,27 @@
 import Service from '@ember/service';
 import require, { has } from 'require';
 
-let _fetch = function () {
-  throw new Error("It seems you have neither fetch nor jQuery available");
-}
+const hasEmberAjax = has('ember-ajax');
+const hasEmberFetch = has('fetch');
 
-if (has('fetch')) {
+let _fetch;
+let _request;
+
+if (hasEmberFetch) {
   // use `fetch` module by default, this is commonly provided by ember-fetch
   _fetch = require('fetch').default;
 } else if (typeof fetch === 'function') {
   // fallback to using global fetch
   _fetch = fetch;
-} 
+} else {
+  _fetch = function() {
+    throw new Error("It seems you have neither fetch nor jQuery available, you may want to `ember install ember-fetch`.");
+  }
+}
 
-const hasJQuery = typeof jQuery !== 'undefined';
+if (hasEmberAjax) {
+  _request = require('ember-ajax/request').default;
+}
 
 export default Service.extend({
   host: 'https://api.mapbox.com',
@@ -21,11 +29,17 @@ export default Service.extend({
   async request(endpoint) {
     let url = this.host + endpoint;
 
-    if (hasJQuery) {
-      return jQuery.getJSON(url);
+    if ( hasEmberFetch ) {
+      return await this._useFetch(url);
+    } else if (hasEmberAjax) {
+      return await _request(url);
     } else {
-      let response = await _fetch(url);
-      return response.json();
+      return await this._useFetch(url);
     }
+  },
+
+  async _useFetch(url) {
+    let response = await _fetch(url);
+    return response.json();    
   }
 });
